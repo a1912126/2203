3b4a6f667812681fe0f5d0d4818b313f03d07cee
root <root@DESKTOP-VAVMOLN.localdomain>
2025-08-23 17:28:42 +0930

    optimise

--- BlackAndRed/BlackAndRed.hpp
+++ BlackAndRed/BlackAndRed.hpp
@@ -4,38 +4,34 @@ class BlackAndRed{
     public:
         int cut(std::string deck){
             int n = int(deck.size());
-            for (int i =0;i<n;i++){
-                int bCnt =0;
-                int rCnt = 0;
-                bool found= true;
-                for (int j =i;j<n;j++){
-                    if (deck[j] == 'R'){
-                        rCnt+=1;
-                    }
-                    else{
-                        bCnt +=1;
-                    }
-                    if (rCnt>bCnt){
-                        found = false;
-                        break;
-                    }
+            int blackCount =0;
+            int redCount = 0;
+            for (char c:deck){
+                if (c=='R'){
+                    redCount +=1;
                 }
-                for (int j =0;j<i;j++){
-                    if (deck[j] == 'R'){
-                        rCnt+=1;
-                    }
-                    else{
-                        bCnt +=1;
-                    }
-                    if (rCnt>bCnt){
-                        found = false;
-                        break;
-                    }
-                }
-                if (found){
-                    return i;
+                else{
+                    blackCount +=1;
                 }
             }
-            return -1;
+            if (redCount>blackCount){
+                return -1;
+            }
+            int res = 0;
+            int cur=0;
+            for (int i =0;i<n;i++){
+                bool found= true;
+                if (deck[i] == 'R'){
+                    cur +=1;
+                }
+                else{
+                    cur -=1;
+                }
+                if (cur >0){
+                    cur = 0;
+                    res = i+1;
+                }
+            }
+            return res;
         }
 };
